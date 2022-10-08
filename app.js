b5fe33ed1b0d54d83d58027fa701bb6694bd64b4
DoniyorShodiyevDoni <109288985+DoniyorShodiyevDoni@users.noreply.github.com>
2022-10-08 17:02:57 +0500

    Cards

---
+++ app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach(item => {
+  item.addEventListener("click", () => {
+    clearActiveClassses();
+    item.classList.add("active");
+  });
+});
+
+function clearActiveClassses(){
+  slides.forEach(item => {
+    item.classList.remove("active");
+  });
+}
